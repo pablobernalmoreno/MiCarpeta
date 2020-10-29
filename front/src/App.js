ad4134e020ecda8a659454fdbe4d0c73a836d71f
pablobernalmoreno <pbm1998@hotmail.com>
2020-10-29 10:56:56 -0500

    basic routing ready

--- front/src/App.js
+++ front/src/App.js
@@ -1,88 +1,36 @@
 import "./App.css";
 import React, { useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { createBrowserHistory as createHistory } from "history";
 import config from "./auth/firebaseConfig";
+import Home from "./views/Home";
+import SignUp from "./views/SignUp";
+import Login from "./views/Login";
+
+export const history = createHistory();
 
 function App() {
-  const [newEmail, setNewEmail] = useState("");
-  const [newPassword, setNewPassword] = useState("");
-  const [oldEmail, setOldEmail] = useState("");
-  const [oldPassword, setOldPassword] = useState("");
+  const [currentUser, setCurrentUser] = useState();
 
-  const handleNewEmail = (event) => {
-    setNewEmail(event.target.value);
-  };
-
-  const handleNewPassword = (event) => {
-    setNewPassword(event.target.value);
-  };
-
-  const handleOldEmail = (event) => {
-    setOldEmail(event.target.value);
-  };
-
-  const handleOldPassword = (event) => {
-    setOldPassword(event.target.value);
-  };
-
-  const handleClickNewUser = () => {
-    config
-      .auth()
-      .createUserWithEmailAndPassword(newEmail, newPassword)
-      .catch(function (error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        console.log(errorCode, " ", errorMessage);
-        // ...
-      });
-  };
-
-  const handleClickSignOut = () => {
-    config
-      .auth()
-      .signOut()
-      .then(function () {
-        // Sign-out successful.
-      })
-      .catch(function (error) {
-        // An error happened.
-      });
-  };
-
-  const handleClickLogin = () => {
-    config
-      .auth()
-      .signInWithEmailAndPassword(oldEmail, oldPassword)
-      .catch(function (error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // ...
-        console.log(errorCode, " ", errorMessage);
-      });
-  };
-
-  console.log(config.auth().currentUser);
+  config.auth().onAuthStateChanged(function (user) {
+    if (user) {
+      setCurrentUser(user.l);
+    } else {
+    }
+  });
+  console.log(currentUser);
   return (
-    <div className="App">
-      <button onClick={handleClickSignOut}>Sign Out</button>
-      <h1>Cuenta nueva</h1>
-      <input type="text" placeholder="Correo nuevo" onChange={handleNewEmail} />
-      <input
-        type="text"
-        placeholder="Contraseña nueva"
-        onChange={handleNewPassword}
-      />
-      <button onClick={handleClickNewUser}>New User</button>
-      <h1>Cuenta vieja</h1>
-      <input type="text" placeholder="Correo viejo" onChange={handleOldEmail} />
-      <input
-        type="text"
-        placeholder="Contraseña vieja"
-        onChange={handleOldPassword}
-      />
-      <button onClick={handleClickLogin}>Old User</button>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/SignUp" exact component={SignUp} />
+        <Route path="/" exact component={Login} />
+        {currentUser !== undefined ? (
+          <Route path="/Home" exact component={Home} />
+        ) : (
+          <Route path="/" exact component={Login} />
+        )}
+      </Switch>
+    </Router>
   );
 }
 
--- front/src/views/Home.js
+++ front/src/views/Home.js
@@ -1,1 +1,23 @@
+import React from "react";
+import config from "../auth/firebaseConfig";
 
+function Home(props) {
+  const handleClickSignOut = () => {
+    config
+      .auth()
+      .signOut()
+      .then((user) => {
+        props.history.push("/");
+      })
+      .catch((error) => {});
+  };
+  return (
+    <div>
+      <h1>Home</h1>
+      <input type="text" />
+      <button onClick={handleClickSignOut}>Sign Out</button>
+    </div>
+  );
+}
+
+export default Home;
